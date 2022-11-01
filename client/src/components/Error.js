6815c699e47e65c01bd91b1ff8076a378e632681
GrantLicata <grant.licata@gmail.com>
2022-10-31 18:54:33 -0700

    Error message styled

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Error = () => {
 
     return (
-        <div className='container mt-3'>
-            <h3>We're sorry, but we could not find the author you are looking for. Would you like to add an author to our database?</h3>
+        <div className='container col-6 mt-3 bg-dark p-4 rounded'>
+            <h3 className='text-white'>We're sorry, but we could not find the author you are looking for. Would you like to add an author to our database?</h3>
             <Link to={'/form'} className="btn btn-primary mt-2">Create an Author</Link>
         </div>
     )
